b2809670c08e606cc0e565cdd63e16c84a082f43
Marty M <github@moontronic.com>
2023-04-10 21:18:01 -0600

    added the funtional tests from the github and updated the tests to work right with the new version of selenium

--- wishlist/travel_wishlist/funtional_tests.py
+++ wishlist/travel_wishlist/funtional_tests.py
@@ -1,56 +1,204 @@
-from selenium.webdriver.firefox.webdriver import WebDriver # i installed firefox webdriver from the apt package manager with 'sudo apt install firefox-geckodriver'
-from selenium.webdriver.common.by import By # updated method to find DOM objects. https://www.selenium.dev/documentation/webdriver/elements/finders/
+# from selenium.webdriver.firefox.webdriver import WebDriver # i installed firefox webdriver from the apt package manager with 'sudo apt install firefox-geckodriver'
+# from selenium.webdriver.common.by import By # updated method to find DOM objects. https://www.selenium.dev/documentation/webdriver/elements/finders/
+
+# from django.test import LiveServerTestCase
+
+# class TitleTest(LiveServerTestCase):
+#     @classmethod  # setup selenium at the start of the test
+#     def setUpClass(cls):
+#         super().setUpClass()
+#         cls.selenium = WebDriver()
+#         cls.selenium.implicitly_wait(10)
+
+#     @classmethod # tear down the selenium instence after testing is done
+#     def tearDownClass(cls): 
+#         cls.selenium.quit()
+#         super().tearDownClass()
+
+
+#     def test_tile_on_home_page(self):
+#         self.selenium.get(self.live_server_url)
+#         self.assertIn('Travel Wishlist',self.selenium.title)
+
+
+# class AddPlacesTest(LiveServerTestCase):
+    
+#     fixtures = ['test_places']
+
+#     @classmethod  # setup selenium at the start of the test
+#     def setUpClass(cls):
+#         super().setUpClass()
+#         cls.selenium = WebDriver()
+#         cls.selenium.implicitly_wait(10)
+
+#     @classmethod # tear down the selenium instence after testing is done
+#     def tearDownClass(cls): 
+#         cls.selenium.quit()
+#         super().tearDownClass()
+
+#     def test_add_new_place(self):
+        
+#         self.selenium.get(self.live_server_url)
+
+#         input_name= self.selenium.find_element(By.ID, 'id_name') # this is the new way to find DOM elements now. https://www.selenium.dev/documentation/webdriver/elements/finders/
+#         input_name.send_keys('Denver')
+
+#         add_button = self.selenium.find_element(By.ID, 'add-new-place')
+#         add_button.click()
+
+#         denver = self.selenium.find_element(By.ID, 'place-name-5')
+#         self.assertEqual('Denver', denver.text)
+
+#         self.assertIn('Denver', self.selenium.page_source) # this still works like it does in the lecture videos 
+#         self.assertIn('New York', self.selenium.page_source)
+#         self.assertIn('Tokyo', self.selenium.page_source)
+
+
+import selenium
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 
 from django.test import LiveServerTestCase
-
 class TitleTest(LiveServerTestCase):
-    @classmethod  # setup selenium at the start of the test
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.selenium = WebDriver()
-        cls.selenium.implicitly_wait(10)
 
-    @classmethod # tear down the selenium instence after testing is done
-    def tearDownClass(cls): 
-        cls.selenium.quit()
-        super().tearDownClass()
+    fixtures = ['test_users']
 
+    def setUp(self):
 
-    def test_tile_on_home_page(self):
-        self.selenium.get(self.live_server_url)
-        self.assertIn('Travel Wishlist',self.selenium.title)
+        self.browser = webdriver.Firefox()  # Change to .Chrome() if using Chrome
+        self.browser.implicitly_wait(3)
 
-
-class AddPlacesTest(LiveServerTestCase):
+        self.browser.get(self.live_server_url + '/admin')   # expect to be redirected to login page 
+        self.browser.find_element(By.ID, 'id_username').send_keys('alice')
+        self.browser.find_element(By.ID, 'id_password').send_keys('qwertyuiop')
+        self.browser.find_element(By.CSS_SELECTOR,'input[type="submit"]').click()
     
-    fixtures = ['test_places']
 
-    @classmethod  # setup selenium at the start of the test
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.selenium = WebDriver()
-        cls.selenium.implicitly_wait(10)
+    def tearDown(self):
+        self.browser.quit()
 
-    @classmethod # tear down the selenium instence after testing is done
-    def tearDownClass(cls): 
-        cls.selenium.quit()
-        super().tearDownClass()
+
+    def test_title_shown_on_home_page(self):    
+        self.browser.get(self.live_server_url)   # get site homepage  
+        self.assertIn(self.browser.title, 'Travel Wishlist')
+
+
+
+class AddEditPlacesTests(LiveServerTestCase):
+
+    fixtures = ['test_users', 'test_places']
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()  
+        self.browser.implicitly_wait(3)
+       
+        self.browser.get(self.live_server_url + '/admin')   # expect to be redirected to login page 
+        self.browser.find_element(By.ID, 'id_username').send_keys('alice')
+        self.browser.find_element(By.ID, 'id_password').send_keys('qwertyuiop')
+        self.browser.find_element(By.CSS_SELECTOR,'input[type="submit"]').click()
+    
+        
+    def tearDown(self):
+        self.browser.quit()
 
     def test_add_new_place(self):
+
+        self.browser.get(self.live_server_url)   # Load home page
+        input_name = self.browser.find_element(By.ID, 'id_name') # find input text box. id was generated by Django forms
+        input_name.send_keys('Denver')  # Enter place name 
+        add_button = self.browser.find_element(By.ID, 'add-new-place')  # Find the add button
+        add_button.click()      # And click it
+
+        # Got to make this test code wait for the server to process the request and for page to reload 
+        # Wait for new element to appear on page
+        wait_for_denver = self.browser.find_element(By.ID, 'place-name-7')
+
+        # Assert wishlist places from test_places are on page
+        self.assertIn('San Francisco', self.browser.page_source)
+        self.assertIn('New York', self.browser.page_source)
+
+        # And the new place too
+        self.assertIn('Denver', self.browser.page_source)
+
+
+    def test_mark_place_as_visited(self):
+
+        self.browser.get(self.live_server_url)  # Load home page
         
-        self.selenium.get(self.live_server_url)
+        # find visited button. It will have the id visited_pk
+        # Where pk = primary key of item. This was configured in the template
+        # In this test, mark New York, pk=2 visited
+        visited_button = self.browser.find_element(By.ID, 'visited-button-2')
+       
+        # new_york = self.browser.find_element(By.ID, 'place-name-2')  # find the place name text 
 
-        input_name= self.selenium.find_element(By.ID, 'id_name') # this is the new way to find DOM elements now. https://www.selenium.dev/documentation/webdriver/elements/finders/
-        input_name.send_keys('Denver')
+        visited_button.click()  # click button 
 
-        add_button = self.selenium.find_element(By.ID, 'add-new-place')
-        add_button.click()
+        # But now page has to reload. How to get Selenium to wait,
+        # And to realize it's a new page, so refresh it's
+        # knowledge of the elements on it?
+        # Can use an Explicit Wait for a particular condition - in this case, the
+        # absence of an element with id = place-name-2
 
-        denver = self.selenium.find_element(By.ID, 'place-name-5')
-        self.assertEqual('Denver', denver.text)
+        wait = WebDriverWait(self.browser, 3)
+        ny_has_gone = wait.until(EC.invisibility_of_element_located((By.ID, 'place-name-2')))
 
-        self.assertIn('Denver', self.selenium.page_source) # this still works like it does in the lecture videos 
-        self.assertIn('New York', self.selenium.page_source)
-        self.assertIn('Tokyo', self.selenium.page_source)
+        # Assert San Francisco is still on page
+        self.assertIn('San Francisco', self.browser.page_source)
+    
+        # But New York is not
+        self.assertNotIn('New York', self.browser.page_source)
+
+        # Load visited page
+        self.browser.get(self.live_server_url + '/visited')
+
+        # New York should now be on the visited page
+        self.assertIn('New York', self.browser.page_source)
+
+        # As well as our other visited places
+    
+        self.assertIn('Tokyo', self.browser.page_source)
+        self.assertIn('Moab', self.browser.page_source)
 
 
+
+class PageContentTests(LiveServerTestCase):
+
+    fixtures = ['test_users', 'test_places']
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()  
+        self.browser.implicitly_wait(3)
+        
+        self.browser.get(self.live_server_url + '/admin')   # expect to be redirected to login page 
+        self.browser.find_element(By.ID, 'id_username').send_keys('alice')
+        self.browser.find_element(By.ID, 'id_password').send_keys('qwertyuiop')
+        self.browser.find_element(By.CSS_SELECTOR, 'input[type="submit"]').click()
+    
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_get_home_page_list_of_places(self):
+
+        self.browser.get(self.live_server_url)
+
+        self.assertIn('San Francisco', self.browser.page_source)
+        self.assertIn('New York', self.browser.page_source)
+
+        self.assertNotIn('Tokyo', self.browser.page_source)
+        self.assertNotIn('Moab', self.browser.page_source)
+    
+
+    def test_get_list_of_visited_places(self):
+
+        self.browser.get(self.live_server_url + '/visited')
+    
+        self.assertIn('Tokyo', self.browser.page_source)
+        self.assertIn('Moab', self.browser.page_source)
+        
+        self.assertNotIn('San Francisco', self.browser.page_source)
+        self.assertNotIn('New York', self.browser.page_source)
